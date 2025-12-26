4ecdf47db33c3a37fe50a089fac0400a4d535f72
Hong Liang, Lam <hongliang.lam@consolsys.intranet>
2025-12-26 18:28:15 +0800

    u

--- src/app/modules/landing/landing.component.ts
+++ src/app/modules/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { LayoutService } from '../../layout/app.layout.service';
 
@@ -7,8 +7,50 @@ import { LayoutService } from '../../layout/app.layout.service';
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit, OnDestroy {
+
+    currentTime: string = '';
+    currentDate: string = '';
+    private intervalId: any;
 
     constructor(public layoutService: LayoutService, public router: Router) { }
 
+    ngOnInit() {
+        this.updateClock();
+        this.intervalId = setInterval(() => {
+            this.updateClock();
+        }, 1000);
+    }
+
+    ngOnDestroy() {
+        if (this.intervalId) {
+            clearInterval(this.intervalId);
+        }
+    }
+
+    updateClock() {
+        const now = new Date();
+
+        // Format time in 12-hour format with seconds
+        const timeOptions: Intl.DateTimeFormatOptions = {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: true
+        };
+        this.currentTime = now.toLocaleTimeString('en-US', timeOptions);
+
+        // Format date with day of week
+        const dateOptions: Intl.DateTimeFormatOptions = {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        };
+        this.currentDate = now.toLocaleDateString('en-US', dateOptions);
+    }
+
+    navigateToLogin() {
+        this.router.navigate(['/auth/login']);
+    }
 }
